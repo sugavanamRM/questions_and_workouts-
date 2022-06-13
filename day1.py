11ca27ff4efec9739d7edea12c4e88faf27ec696
sugavanamRM <sugavanamrs69@.com>
2022-06-13 21:39:26 +0530

    html basics

---
+++ day1.py
@@ -0,0 +1,76 @@
+I - phase
+HTML
+CSS
+Boostrap
+Jquery
+Js
+
+
+<em>sugavanam</em>     ---  style like italic
+
+<p class="check">I am studying HTML</p>
+
+<a href="www.kaniyam.com">welcome to kaniyam</a>
+
+<a href="www.kaniyam.com" title="tamil technology">welcome to kaniyam</a>
+                          tooltip
+<a href="www.kaniyam.com" title="tamil technology" target="_blank">welcome to kaniyam</a>
+
+enter name: <input type="text" disabled="disabled">
+
+enter name: <input type="text" disabled>
+
+enter password: <input type="password">
+
+	
+<h1>header tag is used for heading and sub heading for news or article in content
+it is mainly used to help SEO
+
+BLock level elements:
+	h1, h2 footer
+	
+inline elements:
+	
+empty elements:
+	<img src="">
+	
+<! DOCTYPE HTML>
+<html>
+	<head> container element or block level element
+		<meta charset="utf-8">  <--> {it support all humanlanguage}
+		<title>my home page</title>
+		</head>
+		<body>
+			<p>hi</p>
+			<h1>hello</h1>
+		</body>
+		</html>
+		
+		
+container element:
+	keywords,page description, author name
+	
+	
+properly using symbol in html tag:
+	(character entity references)
+	
+>  greater than  &gt;
+<   less than    &lt;
+"   quotes       &quot;
+'   apostophe    &apos;
+&   ampersand    &amp;
+
+
+<meta charset="utf-8">
+
+meta - data about a data
+
+hindu  paper
+
+100 reporters
+
+reporter : articles
+	
+<meta name="author" content="sudhagar">
+
+<meta name="description" content="selenium">
