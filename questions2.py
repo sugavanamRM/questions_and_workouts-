e0748b3564ecc0cdd32afc9dda276f6031412a26
sugavanamRM <sugavanamrs69@.com>
2022-06-18 15:31:17 +0530

    iv Q

---
+++ questions2.py
@@ -0,0 +1,23 @@
+1) Array is not a datatype.
+
+2)output of ["name!"]*2 --> ['name!', 'name!']
+
+3)insert is not a reserved keyword in python
+
+4)output of >>>4+       --> invalid syntax
+
+5)what will be the output of str[0:] if str='Hello'
+                        -->  'Hell'
+	
+6)floor division        --> //
+
+7)comment               --> #
+
+8)[] is used to access singles character of string
+
+9)x = 4.5
+  y = 2
+	x//y = 2.0
+	
+10)num = set{[1,1,2,3,3,3,4]}
+print(len(num))         -->  invalid syntax
